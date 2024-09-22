74569c083ac02fe4de004ffd7c54e1f6d7fd464d
Zeno Oh <blackroseaaa3@gmail.com>
2024-09-23 05:53:50 +0900

    Add Beakjoon Java form

---
+++ Utils/Forms/백준 Java 양식.java
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/1956
+
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+public class Main {
+    static String INPUT = "input.txt";
+    static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new FileReader(INPUT));
+//        br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
+
+
+/*
+현 시점 실버 1. 제출 950. 정답률 31.761 %
+*/
